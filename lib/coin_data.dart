c7b07f6e2ea0e06fe298ddf999315cf0b22f2a05
Zahraa <Zahraa@Zahraa>
2023-02-13 23:20:13 +0300

    networking/calling API

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -30,3 +30,27 @@ const List<String> cryptoList = [
   'ETH',
   'LTC',
 ];
+
+const mainAPI = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'F164704D-ED55-483D-8FF7-7B67D3615B64';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+
+    for (String crypto in cryptoList) {
+      var url = Uri.parse('$mainAPI/$crypto/$selectedCurrency?apikey=$apiKey');
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        String data = response.body;
+        var decodedData = jsonDecode(data);
+        var lastPrice = decodedData['rate'];
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        //print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
