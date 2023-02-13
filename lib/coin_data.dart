cd602c0a60ac32b87def4e6692a99f4e8f5c783a
Zahraa <Zahraa@Zahraa>
2023-02-13 23:05:36 +0300

    make the cryptocard change on select

---
+++ lib/coin_data.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bit_coin/coin_data.dart';
 import 'package:bit_coin/crypto_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PriceScreen extends StatefulWidget {
@@ -9,6 +11,16 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'AUD';
+
+  List<Text> getPickerItems() {
+    List<Text> items = [];
+    for (String currency in currenciesList) {
+      items.add(Text(currency));
+    }
+    return items;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,22 +35,34 @@ class _PriceScreenState extends State<PriceScreen> {
             children: [
               CryptoCard(
                 cryptoCurrency: 'BTC',
-                selectedCurrency: 'USD',
+                selectedCurrency: selectedCurrency,
+                value: '?',
               ),
               CryptoCard(
                 cryptoCurrency: 'BTC',
-                selectedCurrency: 'USD',
+                selectedCurrency: selectedCurrency,
+                value: '?',
               ),
               CryptoCard(
                 cryptoCurrency: 'BTC',
-                selectedCurrency: 'USD',
+                selectedCurrency: selectedCurrency,
+                value: '?',
               ),
             ],
           ),
           Container(
-            height: 150,
-            color: const Color(0XFF414141),
-          )
+              height: 150,
+              color: const Color(0XFF414141),
+              child: CupertinoPicker(
+                itemExtent: 32.0,
+                onSelectedItemChanged: (int selectedIndex) {
+                  setState(() {
+                    selectedCurrency = currenciesList[selectedIndex];
+                    //getData();
+                  });
+                },
+                children: getPickerItems(),
+              )),
         ],
       ),
     );
